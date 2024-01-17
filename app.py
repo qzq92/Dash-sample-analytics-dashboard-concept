749435d6d709b022ef4feec48f6487bb77a1e4cf
Quek Zhi Qiang <zhi_qiang_quek@aiap.sg>
2024-01-17 18:18:14 +0800

    Updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from helpers.process_static_data import process_traffic_cam_locations
 # Import packages
 from dash import Dash, html
+import dash_leaflet as dl
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_design_kit as ddk
@@ -8,96 +8,200 @@ import plotly.graph_objects as go
 import plotly.express as px
 import sys
 import logging
-import logging.config
-
 
 # Dash instantiation create the dash application using the above layout definition
-app = Dash(__name__,  meta_tags=[{"name": "viewport", "content": "width=device-width"}])
-
-# Plotly mapbox public token
-mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
-
-
-# Traffic_cam_location dataframe initialization
-traffic_cam_locations_df = process_traffic_cam_locations()
-traffic_cam_locations = traffic_cam_locations_df.to_dict("Description")
-
+app = Dash(__name__,
+           meta_tags=[{"name": "viewport", "content": "width=device-width", "initial-scale=1.0"}],
+           external_stylesheets=[dbc.themes.DARKLY],
+           suppress_callback_exceptions = True #
+        )
 
 # Mapbox graph compnment
 mapbox_graph = html.Div(id='div-body',children = [
                     dcc.Graph(id = 'mapbox')
                ])
+ --------------------------------------------------------#
 
-
-# Layout of Dash App
 app.layout = html.Div(
+    id="root",
     children=[
-        # Define a single html div
+        # Header -------------------------------------------------#
         html.Div(
-            className="row",
+            id="header",
             children=[
-                # Column via Div for user controls
                 html.Div(
-                    className="div-user-controls",
-                    children=[
-                        html.P(
-                            """Select location"""
-                        ),
-                        # Change 
+                    [
                         html.Div(
-                            className="row",
-                            children=[
-                                html.Div(
-                                    className="div-for-dropdown",
-                                    children=[
-                                        # Dropdown for locations on map
-                                        dcc.Dropdown(
-                                            id="location-dropdown",
-                                            options=[
-                                                # Set label and value
-                                                {"label": i, "value": i}
-                                                for i in traffic_cam_locations
-                                            ],
-                                            placeholder="Select a location",
+                            [html.H1(children="Analytics Dashboard Demo")],
+                            style={
+                                "display": "inline-block",
+                                "width": "74%",
+                                "padding": "10px 0px 0px 20px",  # top, right, bottom, left
+                            },
+                        ),
+                        html.Div(
+                            [html.H6(children="Created with")],
+                            style={
+                                "display": "inline-block",
+                                "width": "10%",
+                                "textAlign": "right",
+                                "padding": "0px 20px 0px 0px",  # top, right, bottom, left
+                            },
+                        ),
+                        html.Div(
+                            [
+                                html.A(
+                                    [
+                                        html.Img(
+                                            src=app.get_asset_url("dash-logo.png"),
+                                            style={"height": "100%", "width": "100%"},
                                         )
                                     ],
-                                ),
+                                    href="https://plotly.com/",
+                                    target="_blank",
+                                )
                             ],
+                            style={
+                                "display": "inline-block",
+                                "width": "14%",
+                                "textAlign": "right",
+                                "padding": "0px 10px 0px 0px",
+                            },
                         ),
-                    ],
-                ),
-                # Column for app graphs and plots
-                html.Div(
-                    className="div-for-map bg-grey",
-                    children=[
-                        # First children Mapbox graph
-                        dcc.Graph(id="map-graph"),
-                        # Column for analytics 
-                        html.Image(
-                            id="traffic-footage",
-                            className="div-for-traffic-footage bg-grey",
-                        ),
-                    ],
-                ),
-                # Column for analytics 
-                html.Div(
-                    className="div-for-analytics bg-grey",
-                    children=[
-
-
-                        
                     ]
                 ),
             ],
-        )
-    ]
+        ),
+
+        # App Container ------------------------------------------#
+        html.Div(
+            id="app-container",
+            children=[
+                # Left column for map
+                html.Div(
+                    id="left-column",
+                    children=[
+                        html.Div(
+                            id="osm-map-container",
+                            children=[
+                                html.Div(
+                                    [
+                                        html.Div(
+                                            [
+                                                html.H5(Children="Click on anywhere on the map"),
+                                            ],
+                                            style={
+                                                "display": "inline-block",
+                                                "width": "64%",
+                                            },
+                                            className="eight columns",
+                                        ),
+                                    ]
+                                ),
+                                dcc.Graph(id="choropleth"),
+                            ],
+                        ),
+                    ],
+                    style={
+                        "display": "inline-block",
+                        "padding": "20px 10px 10px 40px",
+                        "width": "59%",
+                    },
+                    className="seven columns",
+                ),
+                # Information around the selected point ----------------------#
+                html.Div(
+                    id="graph-container",
+                    children=[
+                        html.Div(
+                            [
+                                dcc.Checklist(
+                                    id="property-type-checklist",
+                                    options=[
+                                        {"label": "F: Flats/Maisonettes", "value": "F"},
+                                        {"label": "T: Terraced", "value": "T"},
+                                        {"label": "S: Semi-Detached", "value": "S"},
+                                        {"label": "D: Detached", "value": "D"},
+                                    ],
+                                    value=["F", "T", "S", "D"],
+                                    labelStyle={"display": "inline-block"},
+                                    inputStyle={"margin-left": "10px"},
+                                ),
+                            ],
+                            style={"textAlign": "right"},
+                        ),
+                        html.Div([dcc.Graph(id="price-time-series")]),
+                    ],
+                    style={
+                        "display": "inline-block",
+                        "padding": "20px 20px 10px 10px",
+                        "width": "39%",
+                    },
+                    className="five columns",
+                ),
+            ],
+            className="row",
+        ),
+        # Other analytics data --------------------------#
+        html.Div(
+            [
+                html.Div(
+                    [dcc.Markdown("Other surrounding information")],
+                    style={
+                        "textAlign": "left",
+                        "padding": "0px 0px 5px 40px",
+                        "width": "69%",
+                    },
+                    className="nine columns",
+                ),
+                html.Div(
+                    id="-container",
+                    children=[
+                        html.Div(
+                            [
+                                dcc.Checklist(
+                                    id="property-type-checklist",
+                                    options=[
+                                        {"label": "F: Flats/Maisonettes", "value": "F"},
+                                        {"label": "T: Terraced", "value": "T"},
+                                        {"label": "S: Semi-Detached", "value": "S"},
+                                        {"label": "D: Detached", "value": "D"},
+                                    ],
+                                    value=["F", "T", "S", "D"],
+                                    labelStyle={"display": "inline-block"},
+                                    inputStyle={"margin-left": "10px"},
+                                ),
+                            ],
+                            style={"textAlign": "right"},
+                        ),
+                        html.Div([dcc.Graph(id="price-time-series")]),
+                    ],
+                    style={
+                        "display": "inline-block",
+                        "padding": "20px 20px 10px 10px",
+                        "width": "39%",
+                    },
+                    className="five columns",
+                ),
+                ),
+            ],
+            className="row",
+        ),
+    ],
 )
+
+
 
 """ start the web application
     the host IP 0.0.0.0 is needed for dockerized version of this dash application
 """
 if __name__ == '__main__':
+    logging.info(sys.version)
+
+    # If running locally in Anaconda env:
+    if "conda-forge" in sys.version:
+        app.run_server(debug=True)
+    else:
+        app.run_server(debug=False, host='0.0.0.0', port=8050)
     # Set app title
     app.title = "Near Real Time Analytics Dashboard"
-    app.run_server(debug=False, host='0.0.0.0', port=8050)
-    server = app.server # required for some deployment environment like Heroku
