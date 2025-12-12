1a853f69cec50ea49be647eb2c2c9b5a0780f7f2
Quek Zhi Qiang <zhi_qiang_quek@aiap.sg>
2025-12-12 22:51:27 +0800

    Updated utility function for map as part of map view standardisation

--- utils/map_utils.py
+++ utils/map_utils.py
@@ -2,6 +2,15 @@
 Map utility functions for common map configurations.
 """
 
+# Standardized Singapore map configuration constants
+# Used across all map components for consistency
+SG_MAP_CENTER = [1.36, 103.82]  # Singapore center coordinates
+SG_MAP_DEFAULT_ZOOM = 12  # Default zoom level
+SG_MAP_BOUNDS = [[1.1304753, 103.6020882], [1.492007, 104.145897]]  # Map bounds to restrict view to Singapore area
+SG_MAP_MIN_ZOOM = 11  # Minimum zoom level
+SG_MAP_MAX_ZOOM = 20  # Maximum zoom level
+ONEMAP_TILES_URL = "https://www.onemap.gov.sg/maps/tiles/Night/{z}/{x}/{y}.png"
+
 
 def get_onemap_attribution():
     """
